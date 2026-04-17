e8f9fe55779fb60ab0db192912188aa5a124c9c3
BYIRINGIRO-Samuel <byiringirosam08@gmail.com>
2026-04-17 23:14:10 +0100

    Add Project Mongoose schema

---
+++ backend/models/Project.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const ProjectSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  category: { type: String, required: true },
+  shortDesc: { type: String, required: true },
+  tech: { type: String, required: true },
+  github: { type: String },
+  demo: { type: String },
+  imageUrl: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Project', ProjectSchema);
