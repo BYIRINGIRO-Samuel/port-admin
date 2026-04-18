da6203966b96d44764b885ad44cd0d18adeeb030
BYIRINGIRO-Samuel <byiringirosam08@gmail.com>
2026-04-18 16:37:13 +0100

    Update toaster position to top-right

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ import Career from './pages/Career';
 function App() {
   return (
     <BrowserRouter>
-      <Toaster position="bottom-right" toastOptions={{ style: { background: '#111', color: '#fff', border: '1px solid rgba(255,255,255,0.1)' } }} />
+      <Toaster position="top-right" toastOptions={{ style: { background: '#111', color: '#fff', border: '1px solid rgba(255,255,255,0.1)' } }} />
       <Routes>
         <Route path="/" element={<Login />} />
         
